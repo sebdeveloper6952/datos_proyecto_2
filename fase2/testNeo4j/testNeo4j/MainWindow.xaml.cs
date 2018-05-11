6c17623a635d81504a46f7cac13a745ee0b1b1a7
sevic69 <sebdeveloper6952@gmail.com>
2018-05-11 16:57:35 -0600

    Add Database building project, made in Visual C#.

---
+++ fase2/testNeo4j/testNeo4j/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testNeo4j
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected const string GAMES_FILE = "clean_games.txt";
+        protected const string PLATFORMS_FILE = "platforms_list.xml";
+        protected const string GENRES_FILE = "genres_list.txt";
+        protected const string PUBLISHERS_FILE = "publishers_list.txt";
+        protected const string GAME_DATA_DELIMITER = "--game--data--finished--";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_LoadPlatforms_Click(object sender, RoutedEventArgs e)
+        {
+            int numberOfPlatforms = 0;
+            using (StreamReader sr = File.OpenText(PLATFORMS_FILE))
+            {
+                string line = sr.ReadLine();
+                string prevLine = string.Empty;
+                while(line != null)
+                {
+                    string[] parts = line.Split('<', '>');
+                    if(parts.Length > 2)
+                    {
+                        if(parts[1].Equals("name"))
+                        {
+                            numberOfPlatforms++;
+                            string[] prevParts = prevLine.Split('<', '>');
+                            neo4jManager.instance.AddPlatform(int.Parse(prevParts[2]), parts[2]);
+                            Console.WriteLine(prevParts[2] + "," + parts[2]);
+                        }
+                    }
+                    // save previous line and advance to next line
+                    prevLine = line;
+                    line = sr.ReadLine();
+                }
+            }
+            MessageBox.Show("Finished writing to db...");
+        }
+
+        private void btn_LoadGames_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = File.OpenText(GAMES_FILE))
+            {
+                string line = sr.ReadLine();
+                Dictionary<string, string> map = new Dictionary<string, string>();
+                while(line != null)
+                {
+                    string[] lineParts = line.Split(':');
+                    map.Add(lineParts[0], lineParts[1]);
+                    line = sr.ReadLine();
+                    // write game to db
+                    if (line == GAME_DATA_DELIMITER)
+                    {
+                        // advance to next line
+                        line = sr.ReadLine();
+                        // write game to db
+                        string id = string.Empty;
+                        string title = string.Empty;
+                        string platformId = string.Empty;
+                        string genres = string.Empty;
+                        string publisher = string.Empty;
+                        map.TryGetValue("id", out id);
+                        map.TryGetValue("GameTitle", out title);
+                        map.TryGetValue("PlatformId", out platformId);
+                        map.TryGetValue("Genres", out genres);
+                        map.TryGetValue("Publisher", out publisher);
+                        string[] gArr = null;
+                        if(genres != null) gArr = genres.Split(',');
+                        neo4jManager.instance.AddGame(int.Parse(id), title, int.Parse(platformId),
+                            gArr, publisher);
+                        map.Clear();
+                    }
+                }
+            }
+            MessageBox.Show("Finished writing games to db...");
+        }
+
+        private void btn_LoadGenres_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = File.OpenText(GENRES_FILE))
+            {
+                List<string> genres = new List<string>();
+                string line = sr.ReadLine();
+                while(line != null)
+                {
+                    if (!genres.Contains(line)) genres.Add(line);
+                    line = sr.ReadLine();
+                }
+                int id = 0;
+                foreach (string genre in genres)
+                {
+                    neo4jManager.instance.AddGenre(id++, genre);
+                }
+            }
+            MessageBox.Show("Finished writing genres...");
+        }
+
+        private void btn_LoadPublishers_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = File.OpenText(PUBLISHERS_FILE))
+            {
+                string line = sr.ReadLine();
+                int id = 1;
+                while(line != null)
+                {
+                    neo4jManager.instance.AddPublisher(id++, line);
+                    line = sr.ReadLine();
+                }
+            }
+            MessageBox.Show("Finished writing to db...");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // close connection to db
+            neo4jManager.instance.Dispose();
+        }
+    }
+}
---
+++ fase2/testNeo4j/testNeo4j/neo4jManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver.V1;
+
+namespace testNeo4j
+{
+    public class neo4jManager : IDisposable
+    {
+        public static neo4jManager instance = new neo4jManager("bolt://localhost:7687", "neo4j", "pass123");
+
+        private readonly IDriver _driver;
+
+        private neo4jManager(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void AddUser(string name)
+        {
+            using (var session = _driver.Session())
+            {
+                var person = session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (a:Person) " +
+                                        "SET a.name = $name " +
+                                        "RETURN a.name",
+                        new { name });
+                    return result.Single()[0].As<string>();
+                });
+                Console.WriteLine(person);
+            }
+        }
+
+        public void AddPlatform(int id, string name)
+        {
+            using (var session = _driver.Session())
+            {
+                session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (p:Platform {id: $id, name: $name})",
+                        new { id, name });
+                });
+            }
+        }
+
+        public void AddGenre(int id, string name)
+        {
+            using (var session = _driver.Session())
+            {
+                session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (gn:Genre {id: $id, name: $name})",
+                        new { id, name });
+                });
+            }
+        }
+
+        public void AddGame(int id, string title, int platformId, string[] genres,
+            string publisher)
+        {
+            using (var session = _driver.Session())
+            {
+                session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (g:Game {id: $id, title: $title})",
+                        new { id, title });
+                });
+            }
+        }
+
+        public void AddPublisher(int id, string name)
+        {
+            using (var session = _driver.Session())
+            {
+                session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (p:Publisher {id: $id, name: $name})",
+                        new { id, name });
+                });
+            }
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
